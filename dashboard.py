0686462e6cfdcf17bdd94fa56e5730d79b625550
AhlemC <96552305+AhlemC@users.noreply.github.com>
2021-12-27 22:28:36 +0100

    Add files via upload

---
+++ dashboard.py
@@ -0,0 +1,102 @@
+
+
+import streamlit as st
+import utils
+
+def main():
+    
+    ########### Initialisation ##############################
+    st.set_page_config(
+        # layout="centered",
+        layout='wide',
+        initial_sidebar_state="collapsed"
+        )
+    loanColumn = 'SK_ID_CURR'
+    dataRef, dataCustomer = utils.loadData()
+    model = utils.loadModel()
+    # threshold = utils.loadThreshold()
+    minScore,maxScore,threshold = utils.loadRatingSystem()
+    
+    ########### Top ##############################
+    col1, col2 = st.beta_columns((1,3))
+    with col1:
+        st.image('img/logo.png', width=200)
+    with col2:
+        st.title('Simulation de prêt')
+        st.header('Obtenez une réponse instantanément')
+        ### Input ###
+        user_input = st.selectbox('Entrez le numéro de prêt:',dataCustomer[loanColumn].tolist())
+        idxCustomer = utils.getTheIDX(dataCustomer,user_input,loanColumn)
+        'Vous avez selectionné le prêt n°: ', user_input,' correspondant au client n°',idxCustomer
+        ### DF des Local Features Importance
+        df = utils.getDFLocalFeaturesImportance(model=model,
+                                        X=dataCustomer,
+                                        loanNumber=int(user_input),
+                                        nbFeatures=12)
+
+    ########### Model Prediction API ##########################
+    predExact, predProba = utils.apiModelPrediction(data=dataCustomer,
+                                                    loanNumber=int(user_input))
+    
+    ########### Loan Validation ##############################
+    st.markdown("# Validation du prêt")
+    loanResult = 'Status du prêt: '
+    if predExact:
+        loanResult += "Validé !"
+        st.success(loanResult)
+    else:
+        loanResult += "Refusé..."
+        st.error(loanResult)
+    
+    
+    ########### Core ##############################
+    col1, col15, col2 = st.beta_columns((2,1,2))
+    with col1:
+        ### Gauge Score
+        fig=utils.gauge_chart(predProba,minScore,maxScore,threshold)
+        st.write(fig)
+    with col15:
+        # Empty column to center the elements
+        st.write("")
+    with col2:
+        ### Img OK/NOK
+        if predExact:
+            st.image('img/ok.png', width=400)
+        else:
+            st.image('img/nok.png', width=450)
+    
+    ### Global & Local Features Importance
+    col1, col2 = st.beta_columns((2))
+    ### Col 1/2 ### Global Features Importance
+    with col1:
+        fig=utils.plotGlobalFeaturesImportance(model, dataRef, 10)
+        st.write(fig)            
+    ### Col 2/2 ### Local Features Importance
+    with col2:
+        fig=utils.plotLocalFeaturesImportance(
+            model=model,
+            X=dataCustomer,
+            loanNumber=int(user_input)
+            )
+        st.write(fig)
+    
+    #### Mono & Bi analysis
+    ### Dist Plot
+    col1, col2 = st.beta_columns((2))
+    
+    with col1:
+        feature1 = st.selectbox('Choisissez la 1ère caractéristique:',df.index, index=0)
+        valueCustomer1 = dataCustomer.loc[dataCustomer[loanColumn]==user_input, feature1].values[0]
+        fig = utils.plotDistOneFeature(dataRef, feature1, valueCustomer1)
+        st.write(fig)
+        
+    with col2:
+        feature2 = st.selectbox('Choisissez la 2nd caractéristique:',df.index, index=1)
+        valueCustomer2 = dataCustomer.loc[dataCustomer[loanColumn]==user_input, feature2].values[0]
+        fig = utils.plotDistOneFeature(dataRef, feature2, valueCustomer2)
+        st.write(fig)
+    
+
+
+if __name__ == "__main__":
+    main()
